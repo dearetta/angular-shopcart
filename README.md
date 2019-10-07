90cf1e25896be9905c70d0c8e8776232fcd787ba
dearetta <dearettaa@gmail.com>
2019-10-07 11:09:35 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-j2turq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-j2turq)
---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, Output, EventEmitter, ActivatedRoute } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alert',
+  templateUrl: './product-alert.component.html',
+  styleUrls: ['./product-alert.component.css']
+})
+export class ProductAlertComponent implements OnInit {
+
+  @Input() product;
+  @Output() notify = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
